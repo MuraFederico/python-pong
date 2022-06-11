c2372d33141e5c8a647799a9ba27f83df5fef372
MuraFederico <federicomura98@gmail.com>
2022-06-11 13:18:40 +0200

    baddles and ball set up

--- pong.py
+++ pong.py
@@ -7,7 +7,38 @@ wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Paddle A
+
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.shapesize(stretch_wid=4,stretch_len=1)
+paddle_a.color('red')
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+# Paddle B
+
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.shapesize(stretch_wid=4,stretch_len=1)
+paddle_b.color('white')
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('circle')
+ball.color('white')
+ball.penup
+ball.goto(0, 0)
 
 # Main Game Loop
 while True:
     wn.update()
+
+
